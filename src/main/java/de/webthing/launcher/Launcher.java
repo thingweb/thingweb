3ae0421b68435a6fb3865f22caa3b465efea903c
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2015-08-29 20:45:10 +0200

    added random changer

--- src/main/java/de/webthing/launcher/Launcher.java
+++ src/main/java/de/webthing/launcher/Launcher.java
@@ -1,7 +1,12 @@
 package de.webthing.launcher;
 
 import de.webthing.servient.ServientBuilder;
+import de.webthing.servient.ThingServer;
 import de.webthing.things.WotGreeter;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
@@ -11,11 +16,24 @@ public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 		ServientBuilder.initialize();
-		
-		ServientBuilder.newThingServer(WotGreeter.newInstance("greeter"));
-		
+
+		ThingServer server = ServientBuilder.newThingServer(WotGreeter.newInstance("greeter"));
+
+		Random rnd = new Random();
+
+		TimerTask valueChanger = new TimerTask() {
+			@Override
+			public void run() {
+				server.setProperty("message", "Hello number " + rnd.nextInt(100));
+			}
+		};
+
+		Timer tiktak = new Timer(true);
+		tiktak.schedule(valueChanger, 500,500);
+
 		ServientBuilder.start();
 		
 		System.in.read();
+		tiktak.cancel();
 	}
 }
