5e075015ac119a024a1fd4dc5182bda33157286c
h0ru5 <johannes.hund@gmail.com>
2015-10-11 15:20:53 +0200

    added libcode for demo

---
+++ demo/get_code.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+curl -O https://gist.githubusercontent.com/h0ru5/97c1c1603598ee18f6e3/raw/a127c2732c59ea281ebec9df41b1584a1d78ccef/Makefile
