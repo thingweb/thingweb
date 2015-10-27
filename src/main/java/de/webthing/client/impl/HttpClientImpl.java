93fabf00aecb3d3b429867dd4ca586174e1f3dba
peintner <daniel.peintner.ext@siemens.com>
2015-10-28 00:30:40 +0100

    change request method parameter to "POST" (instead of "Post")

--- src/main/java/de/webthing/client/impl/HttpClientImpl.java
+++ src/main/java/de/webthing/client/impl/HttpClientImpl.java
@@ -158,7 +158,7 @@ public class HttpClientImpl extends AbstractClientImpl {
 				HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
 				httpCon.setDoOutput(true);
 				httpCon.setRequestProperty("content-type", propertyValue.getMediaType().mediaType);
-				httpCon.setRequestMethod(isAction ? "Post" : "PUT");
+				httpCon.setRequestMethod(isAction ? "POST" : "PUT");
 
 				OutputStream out = httpCon.getOutputStream();
 				out.write(propertyValue.getContent());
