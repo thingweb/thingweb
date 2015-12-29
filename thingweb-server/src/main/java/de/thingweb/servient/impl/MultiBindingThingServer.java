9913f2345a38c5a1c85b64be974fc9a4259e93c2
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2015-12-29 16:35:46 +0100

    added resource for thing description

--- thingweb-server/src/main/java/de/thingweb/servient/impl/MultiBindingThingServer.java
+++ thingweb-server/src/main/java/de/thingweb/servient/impl/MultiBindingThingServer.java
@@ -1,38 +1,40 @@
 /*
- * The MIT License (MIT)
  *
- * Copyright (c) 2015 Siemens AG and the thingweb community
+ *  * The MIT License (MIT)
+ *  *
+ *  * Copyright (c) 2015 Siemens AG and the thingweb community
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  * of this software and associated documentation files (the "Software"), to deal
+ *  * in the Software without restriction, including without limitation the rights
+ *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  * copies of the Software, and to permit persons to whom the Software is
+ *  * furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in
+ *  * all copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  * THE SOFTWARE.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
  */
 
 package de.thingweb.servient.impl;
 
+import de.thingweb.binding.AbstractRESTListener;
 import de.thingweb.binding.RESTListener;
 import de.thingweb.binding.ResourceBuilder;
 import de.thingweb.desc.pojo.ThingDescription;
 import de.thingweb.servient.Defines;
 import de.thingweb.servient.ThingInterface;
 import de.thingweb.servient.ThingServer;
-import de.thingweb.thing.Action;
-import de.thingweb.thing.Property;
-import de.thingweb.thing.Thing;
+import de.thingweb.thing.*;
+import de.thingweb.util.encoding.ContentHelper;
 
 import java.util.*;
 import java.util.logging.Logger;
@@ -148,6 +150,15 @@ public class MultiBindingThingServer implements ThingServer {
         interactionListeners.entrySet().stream().forEachOrdered(
                 entry -> resources.newResource(entry.getKey(), entry.getValue())
         );
+
+        resources.newResource(Defines.BASE_THING_URL + thingModel.getName() + "/.td",
+                new AbstractRESTListener() {
+                    @Override
+                    public Content onGet() {
+                        return ContentHelper.wrap(thingModel.getThingDescription(),
+                                        MediaType.APPLICATION_JSON);
+                    }
+                });
     }
 
 }
