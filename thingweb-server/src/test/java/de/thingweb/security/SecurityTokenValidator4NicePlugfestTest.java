2931aa1372c296fb16b23cfe9721ced55b1a65ce
Johannes Hund <johannes.hund@siemens.com>
2016-01-15 11:31:32 +0100

    tidied security-related code

--- thingweb-server/src/test/java/de/thingweb/security/SecurityTokenValidator4NicePlugfestTest.java
+++ thingweb-server/src/test/java/de/thingweb/security/SecurityTokenValidator4NicePlugfestTest.java
@@ -26,119 +26,244 @@
 
 package de.thingweb.security;
 
-import de.thingweb.servient.TestTools;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Properties;
 
-import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.thingweb.servient.TestTools;
 
 /**
+ * Test class for SecurityTokenValidator4NicePlugfest. This class tests
+ * "minimal" tokens signed with ES256 and HS256. It also tests "normal" tokens
+ * signed with ES256 and HS256
+ * 
  * Created by Johannes on 03.01.2016.
  */
 public class SecurityTokenValidator4NicePlugfestTest {
 
-    private static final String ISSUER = "NicePlugfestAS";
-    private static final String AUDIENCE = "NicePlugfestRS";
-    private static final String PUBKEY_ES256 = "{\"keys\":[{\"kty\": \"EC\",\"d\": \"_hysUUk5sRGAHhl7RJN7x5UhBMiy6pl6kHR5-ZaWzpU\",\"use\": \"sig\",\"crv\": \"P-256\",\"kid\": \"PlugFestNice\",\"x\": \"CQsJZUvJWx5yB5EwuipDXRDye4Ybg0wwqxpGgZtcl3w\",\"y\": \"qzYskD2N7GrGDSgo6N9pPLXMIwr6jowFGyqsTJGmpz4\",\"alg\": \"ES256\"},{\"kty\": \"oct\",\"kid\": \"018c0ae5-4d9b-471b-bfd6-eef314bc7037\",\"use\": \"sig\",\"alg\": \"HS256\",\"k\": \"aEp0WElaMnVTTjVrYlFmYnRUTldicGRtaGtWOEZKRy1PbmJjNm14Q2NZZw==\"}]}";
-    private static final String SUBJECT = "0c5f83a7-cf08-4f48-8337-bfc65ea149ff";
-    private static final String TYPE = "org:w3:wot:jwt:as:min";
+	/*
+	 * Static members to obtain or provide 'as-is' objects/values
+	 */
+	private static final String NICE_PLUGFEST_JWT_TESTVECTOR_FILE = "nice-plugfest-jwt-testvectors.properties";
 
-    private SecurityTokenValidator validator;
-    private TokenRequirementsBuilder builder;
-    private Properties testVectors;
+	private static final String MIN_TOKEN_HS256_KEYWORD = "min-token.HS256";
 
-    @Before
-    public void setUp() throws Exception {
-        builder = TokenRequirements.build()
-                .setIssuer(ISSUER)
-                .setAudience(AUDIENCE)
-                .setVerificationKey(PUBKEY_ES256)
-                .setTokenType(TYPE)
-                .setValidateSignature(true);
-        validator  = new SecurityTokenValidator4NicePlugfest(builder.createTokenRequirements());
-        testVectors = TestTools.loadPropertiesFromResources("nice-plugfest-jwt-testvectors.properties");
-    }
+	private static final String GOOD_CASE_KEYWORD = ".goodCase";
 
-    @Test
-    public void testValidMinimalES256Token() throws Exception {
-    	 testVectors.keySet().parallelStream()
-         .map(key -> (String) key)
-         .filter(key -> key.startsWith("min-token") && key.endsWith(".goodCase"))
-         .forEach((name) -> {
-             String jwt = testVectors.getProperty(name);
-             System.out.println("Key:" + name + "\tToken: " + jwt);
-             try {
-                 validator.checkValidity("GET", "/", jwt);
-                 System.out.println("revoked " + name);
-             } catch (UnauthorizedException e) {
-            	 fail("false positive on " + name);
-             } catch (TokenExpiredException e) {
-                 fail("Unexpected Expiery Exception");
-             }
+	private static final String MIN_TOKEN_ES256_KEYWORD = "min-token.ES256";
+	
+	private static final String VALIDATION_KEY_HS256 = "{\"keys\":[{\"kty\": \"oct\",\"kid\": \"018c0ae5-4d9b-471b-bfd6-eef314bc7037\",\"use\": \"sig\",\"alg\": \"HS256\",\"k\": \"aEp0WElaMnVTTjVrYlFmYnRUTldicGRtaGtWOEZKRy1PbmJjNm14Q2NZZw==\"}]}";
 
-         }
- );
-    }
+	private static final String VALIDATION_KEY_ES256 = "{\"keys\":[{\"kty\": \"EC\",\"d\": \"_hysUUk5sRGAHhl7RJN7x5UhBMiy6pl6kHR5-ZaWzpU\",\"use\": \"sig\",\"crv\": \"P-256\",\"kid\": \"PlugFestNice\",\"x\": \"CQsJZUvJWx5yB5EwuipDXRDye4Ybg0wwqxpGgZtcl3w\",\"y\": \"qzYskD2N7GrGDSgo6N9pPLXMIwr6jowFGyqsTJGmpz4\",\"alg\": \"ES256\"}]}";
 
-    @Test(expected = UnauthorizedException.class)
-    public void testInvalidMinimalES256Token() throws Exception {
-        String jwt = "";
+	/*
+	 * Static members to represent 'expected' objects/values
+	 */
+	private static final String MIN_TOKEN_RESOURCE_DEFAULT = "/";
 
-        //the provided method and uri is irrelevant for now
-        validator.checkValidity("GET", "/", jwt);
-    }
+	private static final String MIN_TOKEN_METHOD_DEFAULT = "GET";
 
-    @Test
-    public void testFailMinTokensES256() throws Exception {
-        //TODO setup
+	private static final String MIN_TOKEN_SUBJECT_DEFAULT = "0c5f83a7-cf08-4f48-8337-bfc65ea149ff";
 
-        testVectors.keySet().parallelStream()
-                .map(key -> (String) key)
-                .filter(key -> key.startsWith("min-token.ES256") && !key.endsWith(".goodCase"))
-                .forEach((name) -> {
-                    String jwt = testVectors.getProperty(name);
-                    System.out.println("Key:" + name + "\tToken: " + jwt);
-                    try {
-                        validator.checkValidity("GET", "/", jwt);
-                        fail("false positive on " + name);
-                    } catch (UnauthorizedException e) {
-                        System.out.println("revoked " + name);
-                    } catch (TokenExpiredException e) {
-                        fail("Unexpected Expiery Exception");
-                    }
+	private static final String MIN_TOKEN_TYPE_DEFAULT = "org:w3:wot:jwt:as:min";
 
-                }
-        );
+	private static final String MIN_TOKEN_ISSUER_DEFAULT = "NicePlugfestAS";
 
-        //commented out to keep build
-        validator.checkValidity("GET","/",testVectors.getProperty("min-token.HS256.goodCase"));
-    }
+	private static final String MIN_TOKEN_AUDIENCE_DEFAULT = "NicePlugfestRS";
 
-    @Test
-    public void testMinTokensHS256() throws Exception {
-        //TODO setup
+	/*
+	 * Instances that represent requirements/validators
+	 */
+	// The TokenRequirementsBuilder instance for "minimal" tokens signed with ES
+	// 256
+	private TokenRequirementsBuilder minTokenES256Reqs;
 
-        testVectors.keySet().parallelStream()
-                .map(key -> (String) key)
-                .filter(key -> key.startsWith("min-token.HS256") && !key.endsWith(".goodCase"))
-                .forEach((name) -> {
-                            String jwt = testVectors.getProperty(name);
-                            System.out.println("Key:" + name + "\tToken: " + jwt);
-                            try {
-                                validator.checkValidity("GET", "/", jwt);
-                                fail("false positive on " + name);
-                            } catch (UnauthorizedException e) {
-                                System.out.println("revoked " + name);
-                            } catch (TokenExpiredException e) {
-                                fail("Unexpected Expiery Exception");
-                            }
+	// The SecurityTokenValidator instance for "minimal" tokens signed with
+	// ES256
+	private SecurityTokenValidator minTokenES256Validator;
 
-                        }
-                );
+	// The TokenRequirementsBuilder instance for "minimal" tokens signed with HS
+	// 256
+	private TokenRequirementsBuilder minTokenHS256Reqs;
 
-        //commented out to keep build
-        validator.checkValidity("GET","/",testVectors.getProperty("min-token.ES256.goodCase"));
-    }
+	// The SecurityTokenValidator instance for "minimal" tokens signed with
+	// HS256
+	private SecurityTokenValidator minTokenHS256Validator;
+
+	// The JWT test vectors
+	private Properties testVectors;
+
+	/**
+	 * Set-up for this test
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		testVectors = TestTools
+				.loadPropertiesFromResources(NICE_PLUGFEST_JWT_TESTVECTOR_FILE);
+		minTokenES256Reqs = TokenRequirements.build()
+				.setIssuer(MIN_TOKEN_ISSUER_DEFAULT)
+				.setAudience(MIN_TOKEN_AUDIENCE_DEFAULT)
+				.setVerificationKey(VALIDATION_KEY_ES256)
+				.setTokenType(MIN_TOKEN_TYPE_DEFAULT)
+				.setValidateSignature(true);
+		minTokenES256Validator = new SecurityTokenValidator4NicePlugfest(
+				minTokenES256Reqs.createTokenRequirements());
+		minTokenHS256Reqs = TokenRequirements.build()
+				.setIssuer(MIN_TOKEN_ISSUER_DEFAULT)
+				.setAudience(MIN_TOKEN_AUDIENCE_DEFAULT)
+				.setVerificationKey(VALIDATION_KEY_HS256)
+				.setTokenType(MIN_TOKEN_TYPE_DEFAULT)
+				.setValidateSignature(true);
+		minTokenHS256Validator = new SecurityTokenValidator4NicePlugfest(
+				minTokenHS256Reqs.createTokenRequirements());
+	}
+
+	/**
+	 * Test minimal tokens signed with ES256 which are expected to be valid
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void validMinimalES256Token() throws Exception {
+		testVectors
+				.keySet()
+				.parallelStream()
+				.map(key -> (String) key)
+				.filter(key -> key.startsWith(MIN_TOKEN_ES256_KEYWORD)
+						&& key.endsWith(GOOD_CASE_KEYWORD))
+				.forEach(
+						(name) -> {
+							String jwt = testVectors.getProperty(name);
+							try {
+								String subClaim = minTokenES256Validator
+										.checkValidity(
+												MIN_TOKEN_METHOD_DEFAULT,
+												MIN_TOKEN_RESOURCE_DEFAULT, jwt);
+								assertEquals(MIN_TOKEN_SUBJECT_DEFAULT,
+										subClaim);
+								System.out.println("Token referred to by key: "
+										+ name
+										+ " is VALID-as expected. Subject: "
+										+ subClaim + " Token details: " + jwt);
+							} catch (UnauthorizedException e) {
+								fail("False negative for " + name);
+							} catch (TokenExpiredException e) {
+								// Minimal tokens do not have a lifetime
+								fail("Unexpected TokenExpiredException");
+							}
+						});
+	}
+
+	/**
+	 * Test minimal tokens signed with HS256 which are expected to be valid
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void validMinimalHS256Token() throws Exception {
+		testVectors
+				.keySet()
+				.parallelStream()
+				.map(key -> (String) key)
+				.filter(key -> key.startsWith(MIN_TOKEN_HS256_KEYWORD)
+						&& key.endsWith(GOOD_CASE_KEYWORD))
+				.forEach(
+						(name) -> {
+							String jwt = testVectors.getProperty(name);
+							try {
+								String subClaim = minTokenHS256Validator
+										.checkValidity(
+												MIN_TOKEN_METHOD_DEFAULT,
+												MIN_TOKEN_RESOURCE_DEFAULT, jwt);
+								assertEquals(MIN_TOKEN_SUBJECT_DEFAULT,
+										subClaim);
+								System.out.println("Token referred to by key: "
+										+ name
+										+ " is VALID-as expected. Subject: "
+										+ subClaim + " Token details: " + jwt);
+							} catch (UnauthorizedException e) {
+								fail("False negative for " + name);
+							} catch (TokenExpiredException e) {
+								// Minimal tokens do not have a lifetime
+								fail("Unexpected TokenExpiredException");
+							}
+						});
+	}
+
+	/**
+	 * Test minimal tokens signed with ES256 which are expected to be invalid
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void invalidMinimalES256Token() throws Exception {
+		testVectors
+				.keySet()
+				.parallelStream()
+				.map(key -> (String) key)
+				.filter(key -> key.startsWith(MIN_TOKEN_ES256_KEYWORD)
+						&& !key.endsWith(GOOD_CASE_KEYWORD))
+				.forEach(
+						(name) -> {
+							String jwt = testVectors.getProperty(name);
+							try {
+								minTokenES256Validator.checkValidity(
+										MIN_TOKEN_METHOD_DEFAULT,
+										MIN_TOKEN_RESOURCE_DEFAULT, jwt);
+								fail("False positive for " + name);
+							} catch (UnauthorizedException e) {
+								System.out
+										.println("Token referred to by key: "
+												+ name
+												+ " is INVALID-as expected. Token details: "
+												+ jwt);
+							} catch (TokenExpiredException e) {
+								// Minimal tokens do not have a lifetime
+								fail("Unexpected TokenExpiredException");
+							}
+						});
+	}
+
+	/**
+	 * Test minimal tokens signed with HS256 which are expected to be invalid
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void invalidMinimalHS256Token() throws Exception {
+		testVectors
+				.keySet()
+				.parallelStream()
+				.map(key -> (String) key)
+				.filter(key -> key.startsWith(MIN_TOKEN_HS256_KEYWORD)
+						&& !key.endsWith(GOOD_CASE_KEYWORD))
+				.forEach(
+						(name) -> {
+							String jwt = testVectors.getProperty(name);
+							try {
+								minTokenHS256Validator.checkValidity(
+										MIN_TOKEN_METHOD_DEFAULT,
+										MIN_TOKEN_RESOURCE_DEFAULT, jwt);
+								fail("False positive on " + name);
+							} catch (UnauthorizedException e) {
+								// Expected
+								System.out
+										.println("Token referred to by key: "
+												+ name
+												+ " is INVALID-as expected. Token details: "
+												+ jwt);
+							} catch (TokenExpiredException e) {
+								// Minimal tokens do not have a lifetime
+								fail("Unexpected TokenExpiredException");
+							}
+						});
+	}
+
+	// TODO: add tests with "normal" tokens signed with ES256 and HS256
 }
