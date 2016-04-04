6dceb873e423174c2172ef288d8db2cb1b2e39fe
danielpeintner <daniel@peintner.org>
2016-04-04 16:16:00 +0200

    TD test with strange characters

--- thingweb-parser/src/test/java/de/thingweb/desc/ThingDescriptionParserTest.java
+++ thingweb-parser/src/test/java/de/thingweb/desc/ThingDescriptionParserTest.java
@@ -40,6 +40,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import de.thingweb.thing.Thing;
+
 public class ThingDescriptionParserTest {
 
     @BeforeClass
@@ -97,7 +99,16 @@ public class ThingDescriptionParserTest {
     	ThingDescriptionParser.fromURL(jsonld);
     	// TODO any further checks?
     }
-
+    
+    @Test
+    public void testLocalStrangeCharacters() throws JsonParseException, IOException {
+    	String foo ="{\"metadata\":{\"name\":\"Ugly strange n\u00E4ime\",\"protocols\":{\"CoAP\":{\"uri\":\"coap://MD1EWQUC/things/ugly+strange+n%c3%a4ime\",\"priority\":1},\"HTTP\":{\"uri\":\"http://MD1EWQUC:8080/things/ugly+strange+n%c3%a4ime\",\"priority\":2}},\"encodings\":[\"JSON\"]},\"interactions\":[{\"@type\":\"Property\",\"name\":\"not url komp\u00E4tibel\",\"writable\":false,\"outputData\":\"xsd:string\"},{\"@type\":\"Action\",\"name\":\"wierdly named \u00E4ktschn\",\"inputData\":null,\"outputData\":\"\"}],\"@context\":\"http://w3c.github.io/wot/w3c-wot-td-context.jsonld\"}";
+   		@SuppressWarnings("unused")
+		Thing td = ThingDescriptionParser.fromBytes(foo.getBytes());
+   		// TODO any further checks?
+    }
+    
+ 
     @Test
     public void testFromFile() {
       String happyPath = "jsonld" + File.separator + "led.v2.jsonld";
