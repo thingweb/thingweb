0cba47ef37b6053b9bae4a2e0af161bb50a68b41
danielpeintner <daniel@peintner.org>
2016-07-25 15:03:42 +0200

    use "default" TokenRequirements in mockup

--- thingweb-server/src/main/java/de/thingweb/mockup/MockupLauncher.java
+++ thingweb-server/src/main/java/de/thingweb/mockup/MockupLauncher.java
@@ -63,7 +63,7 @@ public class MockupLauncher {
 	// TODO should we allow multiple things?
 	public MockupLauncher(final Thing thingDesc) throws Exception {
 		ServientBuilder.initialize();
-		final TokenRequirements tokenRequirements = NicePlugFestTokenReqFactory.createTokenRequirements();
+		final TokenRequirements tokenRequirements = null; // NicePlugFestTokenReqFactory.createTokenRequirements();
 		ThingServer server = ServientBuilder.newThingServer(tokenRequirements);
 
 		ti = server.addThing(thingDesc);
@@ -156,19 +156,19 @@ public class MockupLauncher {
 
 	}
 
-	static class NicePlugFestTokenReqFactory {
-
-		private static final String ISSUER = "NicePlugfestAS";
-		private static final String AUDIENCE = "NicePlugfestRS";
-		private static final String PUBKEY_ES256 = "{\"keys\":[{\"kty\": \"EC\",\"d\": \"_hysUUk5sRGAHhl7RJN7x5UhBMiy6pl6kHR5-ZaWzpU\",\"use\": \"sig\",\"crv\": \"P-256\",\"kid\": \"PlugFestNice\",\"x\": \"CQsJZUvJWx5yB5EwuipDXRDye4Ybg0wwqxpGgZtcl3w\",\"y\": \"qzYskD2N7GrGDSgo6N9pPLXMIwr6jowFGyqsTJGmpz4\",\"alg\": \"ES256\"},{\"kty\": \"oct\",\"kid\": \"018c0ae5-4d9b-471b-bfd6-eef314bc7037\",\"use\": \"sig\",\"alg\": \"HS256\",\"k\": \"aEp0WElaMnVTTjVrYlFmYnRUTldicGRtaGtWOEZKRy1PbmJjNm14Q2NZZw==\"}]}";
-		// private static final String SUBJECT =
-		// "0c5f83a7-cf08-4f48-8337-bfc65ea149ff";
-		private static final String TYPE = "org:w3:wot:jwt:as:min";
-
-		public static TokenRequirements createTokenRequirements() {
-			return TokenRequirements.build().setIssuer(ISSUER).setAudience(AUDIENCE).setVerificationKeys(PUBKEY_ES256)
-					.setTokenType(TYPE).createTokenRequirements();
-		}
-	}
+//	static class NicePlugFestTokenReqFactory {
+//
+//		private static final String ISSUER = "NicePlugfestAS";
+//		private static final String AUDIENCE = "NicePlugfestRS";
+//		private static final String PUBKEY_ES256 = "{\"keys\":[{\"kty\": \"EC\",\"d\": \"_hysUUk5sRGAHhl7RJN7x5UhBMiy6pl6kHR5-ZaWzpU\",\"use\": \"sig\",\"crv\": \"P-256\",\"kid\": \"PlugFestNice\",\"x\": \"CQsJZUvJWx5yB5EwuipDXRDye4Ybg0wwqxpGgZtcl3w\",\"y\": \"qzYskD2N7GrGDSgo6N9pPLXMIwr6jowFGyqsTJGmpz4\",\"alg\": \"ES256\"},{\"kty\": \"oct\",\"kid\": \"018c0ae5-4d9b-471b-bfd6-eef314bc7037\",\"use\": \"sig\",\"alg\": \"HS256\",\"k\": \"aEp0WElaMnVTTjVrYlFmYnRUTldicGRtaGtWOEZKRy1PbmJjNm14Q2NZZw==\"}]}";
+//		// private static final String SUBJECT =
+//		// "0c5f83a7-cf08-4f48-8337-bfc65ea149ff";
+//		private static final String TYPE = "org:w3:wot:jwt:as:min";
+//
+//		public static TokenRequirements createTokenRequirements() {
+//			return TokenRequirements.build().setIssuer(ISSUER).setAudience(AUDIENCE).setVerificationKeys(PUBKEY_ES256)
+//					.setTokenType(TYPE).createTokenRequirements();
+//		}
+//	}
 
 }
