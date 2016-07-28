4c62e01cb3f03299746e8d8cdcb4bd844d3f94a5
danielpeintner <daniel@peintner.org>
2016-07-28 14:55:15 +0200

    Client security tests deactivated till AWS services are back at work

--- thingweb-client/src/test/java/de/thingweb/client/security/TestSecurity4NicePlugfest.java
+++ thingweb-client/src/test/java/de/thingweb/client/security/TestSecurity4NicePlugfest.java
@@ -5,7 +5,13 @@ import junit.framework.TestCase;
 
 public class TestSecurity4NicePlugfest extends TestCase {
 	
+	boolean deactivateSecurityTests = true;
+	
 	public void testES256_1() throws IOException {
+		if(deactivateSecurityTests) {
+			System.err.println("Client security tests deactivated till AWS services are back at work");
+			return;
+		}
 		// ES256
 		Security4NicePlugfest s4p = new Security4NicePlugfest();
 		Registration reg = s4p.requestRegistrationAM();
@@ -16,6 +22,10 @@ public class TestSecurity4NicePlugfest extends TestCase {
 	}
 	
 	public void testHS256_1() throws IOException {
+		if(deactivateSecurityTests) {
+			System.err.println("Client security tests deactivated till AWS services are back at work");
+			return;
+		}
 		// HS256
 		Security4NicePlugfest s4p = new Security4NicePlugfest();
 		Registration regAS = s4p.requestRegistrationAS();
