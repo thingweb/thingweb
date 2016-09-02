8eda30fa925b3526061cbcda089cb2eb9ba424aa
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2016-09-02 23:21:41 +0200

    restricted helper to IPv4 since our Uri serializer does not support IPv6

--- thingweb-server/src/main/java/de/thingweb/binding/BindingTools.java
+++ thingweb-server/src/main/java/de/thingweb/binding/BindingTools.java
@@ -26,18 +26,23 @@
 
 package de.thingweb.binding;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.Collections;
 import java.util.List;
 
 /**
+ * Tools and helper Functions for Protocol Binding
  * Created by Johannes on 06.07.2016.
  */
 public class BindingTools {
 
+
+    /**
+     * Loops through all addresses of all interfaces and returns the first one that is non-local and IPv4
+     * @return candidate IP
+     * @throws UnknownHostException bubbled up from deep withing java, can happen if the hostname is not in /etc/hosts
+     * @throws SocketException something is borked with the socket
+     */
     public static String getIpAddress() throws UnknownHostException, SocketException {
         if(!InetAddress.getLocalHost().isLoopbackAddress())
             return InetAddress.getLocalHost().getHostAddress();
@@ -46,9 +51,10 @@ public class BindingTools {
             for (NetworkInterface intf : interfaces) {
                 List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
                 for (InetAddress addr : addrs) {
-                    if (!addr.isLoopbackAddress()) {
+                    //filter to non-local IPv4 (uri constructor is not coping with v6)
+                    if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
+                        //first catch is the best catch
                         return addr.getHostAddress();
-
                     }
                 }
             }
