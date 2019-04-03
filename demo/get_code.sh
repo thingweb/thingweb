3cc1769a1ed230f94f5124e8f02c7ef45d7fc42d
danielpeintner <daniel@peintner.org>
2019-04-03 08:29:14 +0200

    notify where the project moved to

--- demo/get_code.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/jgarff/rpi_ws281x.git
-cd rpi_ws281x
-curl -O https://gist.githubusercontent.com/h0ru5/97c1c1603598ee18f6e3/raw/a127c2732c59ea281ebec9df41b1584a1d78ccef/Makefile
--- thingweb-javascript/src/main/dist/autorun/greeting.js
+++
@@ -1,2 +0,0 @@
-console.log("Script Engine Running!");
-console.log("and this is an obj: " + JSON.stringify({ "hello" : "world" }));
